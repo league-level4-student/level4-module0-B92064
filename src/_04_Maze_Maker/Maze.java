5147f23fe0e6f9bda77d328dce5703ba6c2d8d07
Brandon Shen <brandom0198@gmail.com>
2021-01-09 17:59:33 -0800

    MazeMaker(IP)

--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -6,26 +6,35 @@ public class Maze {
 
 	private int width;
 	private int height;
-
-	public Maze(int w, int h) {
+	Cell[][] allsquare;
+	
+		public Maze(int w, int h) {
 		this.width = w;
 		this.height = h;
 
 		//2. Initialize the cells using the width and height varibles
-
+		allsquare = new Cell[width][height];
 		//3. Iterated through each cell and initialize it
 		//   using i and j as the location
-		
+		for(int i = 0; i < allsquare.length; i++) {
+			for(int j = 0; j < allsquare[i].length; j++) {
+				allsquare[i][j] = new Cell(i,j);
+			}
+		}
 	}
 
 	//4. This method iterates through the cells and draws them
 	public void draw(Graphics g) {
-		
+		for(int i = 0; i < width; i++) {
+			for(int j = 0; j < height; j++) {
+				allsquare[i][j].draw(g);
+			}
+		}
 	}
 	
 	//4b. This method returns the selected cell.
 	public Cell getCell(int x, int y){
-		return  null;
+		return  allsquare[x][y];
 	}
 
 	public int getWidth() {
